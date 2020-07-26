710b5ab27dd247203d439bfc9b7fa45bf950480f
William Wong <williamwongcastle@gmail.com>
2020-07-26 12:37:29 -0400

    initial commit
    yeah

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Card {
+    string cardName {get; set;}
+    string cardId{get; set;}
+    int cost{get; set;}
+}
