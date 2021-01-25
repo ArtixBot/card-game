0701a7fd31f907669f074e749895b7b86e8ea3cc
William Wong <williamwongcastle@gmail.com>
2021-01-24 20:29:36 -0500

    adds damage types
    piercing damage bypasses block, static damage ignores all damage modifiers

--- Assets/Scripts/Combat Actions/DamageTargetAction.cs
+++ Assets/Scripts/Combat Actions/DamageTargetAction.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Damage types. Piercing damage ignores block, while Static damage isn't affected by damage modifiers (positive or negative).
+public enum DamageType {NORMAL, PIERCING, STATIC};
+
 // Damage a target.
 public class DamageTargetAction : AbstractAction {
+
+    public bool isPiercing = false;
     public int damageValue;
 
-    public DamageTargetAction(AbstractCharacter source, AbstractCharacter target, int damageValue){
-        this.source = source;
-        this.target = target;
-        int sourceDamageValue = (int)Mathf.Round((damageValue + source.damageDealtMod) * source.damageDealtMul);
-        int targetDamageTaken = (int)Mathf.Round((sourceDamageValue + target.damageTakenMod) * target.damageTakenMul);
-        this.damageValue = Mathf.Max(0, targetDamageTaken);     // Damage dealt cannot be below 0.
+    // Defaults to normal-type damage.
+    public DamageTargetAction(AbstractCharacter source, AbstractCharacter target, int damageValue, DamageType damageType = DamageType.NORMAL){
+        if (damageType == DamageType.NORMAL || damageType == DamageType.PIERCING){
+            this.source = source;
+            this.target = target;
+            this.isPiercing = (damageType == DamageType.PIERCING);
+            int sourceDamageValue = (int)Mathf.Round((damageValue + source.damageDealtMod) * source.damageDealtMul);
+            int targetDamageTaken = (int)Mathf.Round((sourceDamageValue + target.damageTakenMod) * target.damageTakenMul);
+            this.damageValue = Mathf.Max(0, targetDamageTaken);     // Damage dealt cannot be below 0.
+        } else if (damageType == DamageType.STATIC){
+            this.damageValue = damageValue;
+        }
     }
 
     public override void Resolve(){
-        if (this.damageValue > this.target.def){
-            this.target.def = 0;
-            this.target.curHP -= (this.damageValue - this.target.def);
+        if (this.isPiercing){
+            this.target.curHP -= this.damageValue;
         } else {
-            this.target.def -= this.damageValue;
+            if (this.damageValue > this.target.def){
+                this.target.def = 0;
+                this.target.curHP -= (this.damageValue - this.target.def);
+            } else {
+                this.target.def -= this.damageValue;
+            }
         }
     }
 }
